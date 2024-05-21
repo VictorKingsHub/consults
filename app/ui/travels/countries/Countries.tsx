d65b62fef7d6f8f6c33d370747cdf634f5811c22
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-05-21 11:28:29 +0100

    usestate fixed

--- app/ui/travels/countries/Countries.tsx
+++ app/ui/travels/countries/Countries.tsx
@@ -1,13 +1,11 @@
 "use client"
-import { COUNTRIES_OPTIONS } from '@/app/constants'
 import React, { useState } from 'react'
-import styles from "./Countries.module.css"
 import Image from 'next/image'
-
-const [selected, seSelected] = useState()
+import { COUNTRIES_OPTIONS } from '@/app/constants'
+import styles from "./Countries.module.css"
 
 const CountryDropdown = () => {
-
+  const [selected, seSelected] = useState()
 
   return (
     <div className={styles.container}>
@@ -17,18 +15,14 @@ const CountryDropdown = () => {
               <option value={country.name} key={country.id}> { selected && country.name} </option>
           )
         })}
-      </select>
-      
+      </select>      
       <div className="styles image">
-       
       {COUNTRIES_OPTIONS.map((image: any) => {
         return (
           <Image src={image.photo} width={300} height={300} alt={image.name} key={image.key}/>
         )
       })}
-
       </div>
-
     </div>
   )
 }
