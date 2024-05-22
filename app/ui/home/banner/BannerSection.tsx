a5ad04c0a8e47fdcd9c26a0834d798fab6ed8284
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-05-22 14:32:13 +0100

    Footer Image replaced

--- app/ui/home/banner/BannerSection.tsx
+++ app/ui/home/banner/BannerSection.tsx
@@ -10,16 +10,16 @@ const BannerSection = () => {
     <div className={styles.container}>
       <div className={styles.first} >
         <div className={styles.firstTitle}>Travel And Tour</div>
-        <div className={styles.firstIcon} ><FaAngleDoubleRight /></div>
+        <div className={styles.icon} ><FaAngleDoubleRight /></div>
       </div>
       <div className={styles.second}>
         <div className={styles.up}>
           <div className={styles.upTitle}>Degrees</div>
-          <div className={styles.upIcon}><FaAngleDoubleRight /></div>
+          <div className={styles.icon}><FaAngleDoubleRight /></div>
         </div>
         <div className={styles.down}>
           <div className={styles.downTitle}> Relaxation</div>
-          <div className={styles.downIcon}><FaAngleDoubleRight /></div>
+          <div className={styles.icon}><FaAngleDoubleRight /></div>
         </div>
       </div>
     </div>
