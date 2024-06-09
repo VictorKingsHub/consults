586294ba566e325bd9b5386dd423841b7e91f304
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 15:53:16 +0100

    h

--- app/components/accordionMenu/data.ts
+++ app/components/accordionMenu/data.ts
@@ -1,7 +1,7 @@
 //  import travelInsurance from "@/public/globe.jpg"
 
  const data = [
-    {id: 1, question: "School Serviices", title: "Educational Consulting", answer: "Teachers Capacity Building and Workshop, School Supervision, Management and Teacher's recruiting.", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 1, question: "School Services", title: "Educational Consulting", answer: "Teachers Capacity Building and Workshop, School Supervision, Management and Teacher's recruiting.", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
 
     {id: 2, question: "School Upgrade", title: "Teachers' staffing", answer: "Accreditation and Upgrading for new and existing schools, Curriculum design and development.", img: "/assurance.png" , link: "/travels/insurance", button: "Learn More"},
 
