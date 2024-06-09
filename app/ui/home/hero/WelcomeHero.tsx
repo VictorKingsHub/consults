9f7215d687701b56b14ed785343a732d78403c62
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 14:32:51 +0100

    carousel delay

--- app/ui/home/hero/WelcomeHero.tsx
+++ app/ui/home/hero/WelcomeHero.tsx
@@ -10,18 +10,18 @@ const WelcomeHero = () => {
     return (
         <div className={styles.container}>
             <div className={styles.text}>
-                <span className={styles.welcome}>Welcome</span>
+                {/* <span className={styles.welcome}>Welcome</span> */}
                 <h1 className={styles.header}>Educational Placement and Travel & Study Abroad</h1>
                 <p className={styles.description}>
-                    
-                    {/* We provide services to a large clientele seeking academic pursuit overseas and families pursuing working and resident permit Abroad */}
-                    
                     We provide educational consultancy services for schools in Nigeria, supporting academic success and school supervision. We also offer international travel opportunities for students, individuals and groups, promoting cultural exchange and exploration.
-
-                    </p>
+                </p>
                 <div className={styles.buttons}>
-                    <ButtonBackground text={'Educational Consulting'} link={'/education'} />
-                    <Button text={'Study and Work abroad'} link={'/travels'} />
+                    <div className={styles.btn}>
+                        <ButtonBackground text={'Educational Consulting'} link={'/education'} />
+                    </div>
+                    <div className={styles.btn}>
+                        <Button text={'Study and Work abroad'} link={'/travels'} />
+                    </div>
                 </div>
                 {/* <div className={styles.img}>
                     <Image src={photo4} alt='plane' width={250} height={250} className={styles.imgplane} />
