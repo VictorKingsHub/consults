c9e334e0c0a08332498ce0a4493887ca064e56e5
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 14:16:01 +0100

    carousel delay

--- app/ui/travels/services/TravelServices.tsx
+++ app/ui/travels/services/TravelServices.tsx
@@ -18,9 +18,9 @@ const TravelServicesPage = () => {
         <div className={styles.container}>
             <div className={styles.popular}>
                 <div className={styles.toplabel}> Our Top Services</div>
-                <div className={styles.topDesc}>
+                {/* <div className={styles.topDesc}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vero fugiat quisquam excepturi adipisci tempore nam iste, molestiae at saepe!  molestiae at saepe!  molestiae at saepe!
-                </div>
+                </div> */}
             </div>
 
             <div className={styles.wrapper}>
@@ -40,7 +40,7 @@ const TravelServicesPage = () => {
                             <h1
                                 className={` ${selected === serviceItem.id ? styles.activeLabel : styles.label} `}
                             >{serviceItem.label}</h1>
-                            <p className={styles.desc}> {serviceItem.description} </p>
+                            {/* <p className={styles.desc}> {serviceItem.description} </p> */}
                         </div>
                     )
                 })}
