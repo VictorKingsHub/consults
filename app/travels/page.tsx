f500e8a06843270f34d9bc4a6445de875f2515aa
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 12:40:14 +0100

    make enquiry

--- app/travels/page.tsx
+++ app/travels/page.tsx
@@ -38,7 +38,7 @@ const TravelsPage = () => {
        <TravelServicesPage />
 
       <WhyChooseUs
-        img={"/man.jpg"}
+        img={"/amen.jpg"}
         icon1={<FaUserGraduate />}
         icon2={<RiSpeakFill />}
         icon3={<GiHumanPyramid />}
