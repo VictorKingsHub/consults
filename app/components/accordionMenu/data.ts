2ef2469fa55b2c16b248adbe6c921673cbb1cf0b
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 15:33:02 +0100

    footer gap

--- app/components/accordionMenu/data.ts
+++ app/components/accordionMenu/data.ts
@@ -1,15 +1,12 @@
 //  import travelInsurance from "@/public/globe.jpg"
 
  const data = [
-    {id: 1, question: "Educational Consulting", title: "Educational Consulting", answer: "Riskout Travels conducts comprehensive risk assessments to identify potential hazards and threatsns, transportation, and activities", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 1, question: "SCHOOL SERVICES", title: "Educational Consulting", answer: "Teachers Capacity Building and Workshop, School Supervision, Management and Teacher's recruiting.", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
 
-    {id: 2, question: "Teachers' staffing", title: "Teachers' staffing", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeseen events like", img: "/assurance.png" , link: "/travels/insurance", button: "Learn More"},
+    {id: 2, question: "SCHOOL UPGRADE", title: "Teachers' staffing", answer: "Accreditation and Upgrading for new and existing schools, Curriculum design and development.", img: "/assurance.png" , link: "/travels/insurance", button: "Learn More"},
 
-    {id: 3, question: "Cambridge Curriculum", title: "Cambridge Curriculum", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeds", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
-    // {id: 4, question: "Risk Management", title: "Risk Management", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeds", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 3, question: "TEACHING MATERIALS", title: "Cambridge Curriculum", answer: "Supply of Teaching and learning aids", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
     
-    {id: 4, question: "Trained Teachers Academy", title: "Trained Teachers Academy", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeds", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
-    // {id: 5, question: "Scholarships", title: "Scholarships", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeds", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
-    // {id: 6, question: "Web Design & Development", title: "Web Design & Development", answer: "Riskout Travels assists travelers in selecting the right travel insurance policies to protect them against unforeds", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 4, question: "TRAINED TEACHERS ACADEMY", title: "Trained Teachers Academy", answer: "We give professional guides and assistance through out your apllication and study experience", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
 ]
 export default data
