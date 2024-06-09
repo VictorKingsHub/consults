e6bc391bb9b2de2f9e1a9d4378150b639d5ba9c0
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 15:35:24 +0100

    mQuestionMenu changing

--- app/components/accordionMenu/data.ts
+++ app/components/accordionMenu/data.ts
@@ -1,12 +1,12 @@
 //  import travelInsurance from "@/public/globe.jpg"
 
  const data = [
-    {id: 1, question: "SCHOOL SERVICES", title: "Educational Consulting", answer: "Teachers Capacity Building and Workshop, School Supervision, Management and Teacher's recruiting.", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 1, question: "School Serviices", title: "Educational Consulting", answer: "Teachers Capacity Building and Workshop, School Supervision, Management and Teacher's recruiting.", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
 
-    {id: 2, question: "SCHOOL UPGRADE", title: "Teachers' staffing", answer: "Accreditation and Upgrading for new and existing schools, Curriculum design and development.", img: "/assurance.png" , link: "/travels/insurance", button: "Learn More"},
+    {id: 2, question: "School Upgrade", title: "Teachers' staffing", answer: "Accreditation and Upgrading for new and existing schools, Curriculum design and development.", img: "/assurance.png" , link: "/travels/insurance", button: "Learn More"},
 
-    {id: 3, question: "TEACHING MATERIALS", title: "Cambridge Curriculum", answer: "Supply of Teaching and learning aids", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 3, question: "Teaching Materials", title: "Cambridge Curriculum", answer: "Supply of Teaching and learning aids", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
     
-    {id: 4, question: "TRAINED TEACHERS ACADEMY", title: "Trained Teachers Academy", answer: "We give professional guides and assistance through out your apllication and study experience", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
+    {id: 4, question: "Trained Teachers Academy", title: "Trained Teachers Academy", answer: "We give professional guides and assistance through out your apllication and study experience", img: "/risk.png", link: "/travels/insurance", button: "Learn More"},
 ]
 export default data
