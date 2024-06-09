ef3cc068e474da14b128dfada9d6307aeaaf4424
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-06-09 13:17:13 +0100

    WhoweAre Hero text

--- app/ui/whoWeAre/us/Us.tsx
+++ app/ui/whoWeAre/us/Us.tsx
@@ -22,10 +22,10 @@ interface LeftSectionProps {
 export default function UsSection() {
     return (
         <div className={styles.container}>
-            <RightSection vissionTitle={'Our Vision'} coreValueTitle={''} vissionImage={"/team1.jpg"} coreValueImage={undefined} vissionDescription={'We are Nigerian based firm that is into Human Capacity, Seminars and workshops. We train staff of companies, Teachers in private and public schools, school proprietors and Administrators, bankers, Health Officials and Personnel, Staff of security Agencies etc.'} vissionImageAlt={'Vission Image'} />
+            <RightSection vissionTitle={'Our Vision'} coreValueTitle={''} vissionImage={"/team1.jpg"} coreValueImage={undefined} vissionDescription={'Our vision is to bridge cultures, connect minds, and empower futures. We envision a world where education, travel, and career opportunities know no borders, and where individuals can reach their full potential. We strive to be the catalyst for transformative experiences that inspire growth, understanding, and success.'} vissionImageAlt={'Vission Image'} />
 
 
-            <LeftSection missionTitle={'Our Mission'} missionDescription={'We are Nigerian based firm that is into Human Capacity, Seminars and workshops. We train staff of companies, Teachers in private and public schools, school proprietors and Administrators, bankers, Health Officials and Personnel, Staff of security Agencies etc.'} missionImage={"/team1.jpg"} missionImageAlt={'mission Image'} />
+            <LeftSection missionTitle={'Our Mission'} missionDescription={'Our mission is to empower students, educators, and professionals to reach their full potential. With our expertise and dedication, we strive to deliver exceptional services that exceed expectations.'} missionImage={"/team1.jpg"} missionImageAlt={'mission Image'} />
 
             <RightSection
                 coreValueTitle={'Our Core values'}
